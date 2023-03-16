a2467ef57d467e90029fcd70a6f8924f151eb959
DMS473 <daffamuis0407@gmail.com>
2023-03-17 06:08:24 +0700

    first commit

---
+++ luasSegitiga.py
@@ -0,0 +1,15 @@
+# alas = input("Berapa panjang alas: ");
+# tinggi = input("Berapa panjang tinggi: ");
+
+# luas = alas + tinggi
+
+# print(luas)
+
+
+# Ambil input untuk mengisi nilai
+a = input("Inputkan nilai a: ")
+b = input("Inputkan nilai b: ")
+
+# Menggunakan operator penjumlahan
+c = a + b
+print ("Hasil %d + %d = %d") % (a,b,c)
